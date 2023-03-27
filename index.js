32922faae66c6c4fedfa7d1c6b225b05683582e6
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-03-26 20:11:22 -0500

    completion of function continued lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (thingToDo = `go to the office`){
+    return `This Monday, I will ${thingToDo}.`;
+}
+function wrapAdjective(specialSign ="*"){
+    const specialMessage = function (alwaysThis = "special"){
+        return `You are ${specialSign}${alwaysThis}${specialSign}!`;
+    }
+ return specialMessage;
+}
